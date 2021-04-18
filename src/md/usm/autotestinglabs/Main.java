59d7ff55f33497a54e5b79179fca21e3c1fd9287
SergheiKot <sergheikotwork@gmail.com>
2021-04-18 17:44:01 +0300

    Added lab-2

--- src/md/usm/autotestinglabs/Main.java
+++ src/md/usm/autotestinglabs/Main.java
@@ -1,7 +1,23 @@
 package md.usm.autotestinglabs;
 
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (CSVReader reader = new CSVReader(new FileReader("D:\\Data\\input.csv"), ',')) {
+
+            String[] nextLine;
+
+            while ((nextLine = reader.readNext()) != null) {
+
+                for (String token : nextLine) {
+                    System.out.println(token);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
